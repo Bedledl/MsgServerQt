6cdb845b296a6f65ad47bd14a454a5854560990b
Bettina Riedl <bettina.riedl@zuhlke.com>
2024-01-18 15:42:02 +0100

    Add GlobalChat Singleton

--- server/include/chat.h
+++ server/include/chat.h
@@ -22,4 +22,23 @@ public:
     const UniqueKey<Chat> &get_key() const { return key; };
 };
 
+/// @brief Global Chat as Singleton, because we want to have exactly one instance available over the lifetime of the server.
+class GlobalChat : public Chat
+{
+public:
+    static GlobalChat &get_instance()
+    {
+        static GlobalChat instance;
+        volatile int8_t dummy;
+        dummy; // just for removeing the unused warning. This variable is for preventing the optimizing away the instance
+        return instance;
+    }
+
+private:
+    GlobalChat() = default;
+    ~GlobalChat() = default;
+    GlobalChat(const GlobalChat &) = delete;
+    GlobalChat &operator=(const GlobalChat &) = delete;
+};
+
 #endif
