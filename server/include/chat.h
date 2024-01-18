9a866c78ab9425abcf9edfd77d17fbddecc10aba
Bettina Riedl <bettina.riedl@zuhlke.com>
2024-01-18 15:41:41 +0100

    Reformat chat.h

--- server/include/chat.h
+++ server/include/chat.h
@@ -1,18 +1,25 @@
-#include <string>
-#include <set>
+#ifndef CHAT_H
+#define CHAT_H
+
 #include <QReadWriteLock>
 #include <functional>
+#include <set>
 
-#include "message.h"
 #include "clientOrganizer.h"
+#include "message.h"
+#include "uniqueKey.h"
 
-
-class Chat {
+class Chat
+{
     const UniqueKey<Chat> key;
     std::set<Message> messages = {};
     QReadWriteLock lock;
-    public:
-    void foreach_do(std::function<void(const Message&)> func);
+
+public:
+    Chat(){};
+    void foreach_do(std::function<void(const Message &)> func);
     void add_message(Message msg);
     const UniqueKey<Chat> &get_key() const { return key; };
 };
+
+#endif
