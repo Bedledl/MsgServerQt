27392e035d92a0aa14e704c7c92185f0a9d1813a
Bettina Riedl <bettina.riedl@zuhlke.com>
2024-01-18 15:29:19 +0100

    add correct and strong ordering to Message

--- server/include/message.h
+++ server/include/message.h
@@ -2,10 +2,28 @@
 #include <chrono>
 #include <compare>
 
-class Message {
+class Message
+{
     std::chrono::time_point<std::chrono::system_clock> date;
     QString content;
-    public:
-    Message(QString content) : date(std::chrono::system_clock::now()), content(content) {};
-    auto operator<=>(const Message &msg) const { return date <=> msg.date; };
+
+public:
+    Message(QString content) : date(std::chrono::system_clock::now()), content(content){};
+    std::strong_ordering operator<=>(const Message &msg) const
+    {
+        if (date == msg.date)
+        {
+            auto string_compare = QString::compare(content, msg.content, Qt::CaseSensitive);
+            if (string_compare == 0)
+            {
+                return std::strong_ordering::equal;
+            }
+            else if (string_compare < 0)
+            {
+                return std::strong_ordering::less;
+            }
+            return std::strong_ordering::greater;
+        }
+        return date <=> msg.date;
+    };
 };
