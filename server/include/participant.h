5bed938e574670517cc96566cb4bef126582600e
Bettina Riedl <bettina.riedl@zuhlke.com>
2024-01-18 15:30:17 +0100

    Enhance Participant class
        - add UniqueKey
        - add getter methods for key
        - correct constructor
        - formatting

--- server/include/participant.h
+++ server/include/participant.h
@@ -1,10 +1,19 @@
+#include <QDateTime>
+#include <QString>
 
+#include "uniqueKey.h"
 
-typedef unsigned participant_key;
-
-class Participant {
+class Participant
+{
+        const UniqueKey<Participant> key;
         QString name;
-        QDateTime registered_since;
-        public:
-        Participant(QString &name, QDateTime &registered_since) : name(name), registered_since(registered_since) {};
+        const QDateTime registered_since;
+
+public:
+        Participant() : registered_since(QDateTime::currentDateTime())
+        {
+                name = "Unknown";
+        };
+        void setName(QString new_name) { name = new_name; };
+        unsigned getKey() const { return key.getKey(); }
 };
