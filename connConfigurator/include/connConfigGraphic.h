01b2865b4df5a87fda5e4b14c44514e117cbe229
Bettina Riedl <bettina.riedl@zuhlke.com>
2024-01-25 15:34:20 +0100

    fixup! add Configurators for Network Iface, IP and Port

--- connConfigurator/include/connConfigGraphic.h
+++ connConfigurator/include/connConfigGraphic.h
@@ -10,11 +10,6 @@ class ConnConfiguratorGraphic : public QDialog, public ConnConfigurator
 {
     Q_OBJECT
 
-    std::unique_ptr<QComboBox> ifaceBox;
-    std::unique_ptr<QComboBox> addrBox;
-    std::unique_ptr<QLineEdit> portLineEdit;
-    std::unique_ptr<QPushButton> connectButton;
-
 private slots:
     void ifaceSet(int index);
     void tryEnableConnectButton();
@@ -26,6 +21,13 @@ public:
     ConnConfiguratorGraphic(QWidget *parent = nullptr);
     ~ConnConfiguratorGraphic();
     std::pair<QString, quint16> retrieveConnectionConfiguration() override;
+
+protected:
+    std::unique_ptr<QComboBox> ifaceBox;
+    std::unique_ptr<QComboBox> addrBox;
+    std::unique_ptr<QLineEdit> portLineEdit;
+    std::unique_ptr<QPushButton> connectButton;
+
 public slots:
     void displayError(std::exception &exc);
 };
