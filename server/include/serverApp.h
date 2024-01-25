3209402f73a25eac0c0a4dc3a84ea75d6d38cab9
Bettina Riedl <bettina.riedl@zuhlke.com>
2024-01-25 14:47:09 +0100

    Add ServerApplication inheriting appGui

--- server/include/serverApp.h
+++ server/include/serverApp.h
@@ -1,20 +1,26 @@
 #ifndef SERVER_APP_H
 #define SERVER_APP_H
 
+#include <QComboBox>
+#include <QLabel>
 #include <QWidget>
 
+#include "appGui.h"
 #include "server.h"
 
-//#include "serverApp.moc"
+// #include "serverApp.moc"
 
-class ServerApplication : public QWidget {
+class ServerApplication : public AppGui
+{
     Q_OBJECT
 
-    public:
-    ServerApplication(QWidget *parent = nullptr);
-    private:
-    Server *server = NULL;
-    ChatParticipantOrganizer *chat_organizer = NULL;
+public:
+    ServerApplication(std::unique_ptr<Server> server, QWidget *parent = nullptr);
+
+private:
+    std::unique_ptr<Server> server;
+    std::unique_ptr<QLabel> status_label;
+    std::unique_ptr<QComboBox> hostCombo;
 };
 
 #endif
