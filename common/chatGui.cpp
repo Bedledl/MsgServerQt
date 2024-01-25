e208b8b636e6bf014a9e5448e1aef13ffd0d0ddf
Bettina Riedl <bettina.riedl@zuhlke.com>
2024-01-25 14:42:14 +0100

    add chat gui

---
+++ common/chatGui.cpp
@@ -0,0 +1,22 @@
+#include "chatGui.h"
+#include "moc_chatGui.cpp"
+#include <iostream>
+
+void ChatGui::chatsChanged()
+{
+    message_texts.clear();
+    if (chat)
+    {
+        chat->foreach_do([&](const Message &msg)
+                         { message_texts.append(new QLabel(msg.toString())); });
+    }
+}
+
+void ChatGui::updateMessages()
+{
+}
+
+ChatGui::ChatGui(QWidget *parent, Chat *chat) : QWidget(parent), chat(chat)
+{
+    std::cout << "Chat Gui Constructor" << std::endl;
+}
