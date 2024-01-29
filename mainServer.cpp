7294b06d2b7c1e7cf5288542889079478c11bf65
Bettina Riedl <bettina.riedl@zuhlke.com>
2024-01-29 11:50:18 +0100

    Configure server connection depending on command line parameters

--- mainServer.cpp
+++ mainServer.cpp
@@ -1,39 +1,48 @@
 #include "chat.h"
+#include "configParser.h"
 #include "connConfigGraphic.h"
 #include "server/include/serverApp.h"
 #include "tcpserver.h"
 #include <QApplication>
 #include <iostream>
 
+#include "connConfigFile.h"
+#include <QCommandLineParser>
+#include <QDir>
+
 int main(int argc, char *argv[])
 {
+
     QApplication app(argc, argv);
     QApplication::setApplicationDisplayName("TCP Message Server");
+    QApplication::setApplicationName("LaunchServer");
+    QApplication::setApplicationVersion("1.0");
 
-    ConnConfiguratorGraphic config;
-    config.show();
+    QString configFileCmdLineOptName = "config-file";
+    QCommandLineParser parser;
+    configureParser(parser, configFileCmdLineOptName);
+    parser.process(app);
+
+    std::unique_ptr<ConnConfigurator> connConfigurator = createConnConfiguratorFromSettings(parser, configFileCmdLineOptName);
+
+    // may block until conneciton configuration is retrieved
+    auto [ip, port] = connConfigurator->retrieveConnectionConfiguration();
 
     std::unique_ptr<ServerApplication> serverApp;
 
-    QObject::connect(&config, &ConnConfiguratorGraphic::configReady,
-                     [&]()
-                     {
-                         try
-                         {
-                             auto [ip, port] = config.retrieveConnectionConfiguration();
-                             serverApp = std::make_unique<ServerApplication>(std::make_unique<TCPMessageServer>(QHostAddress(ip), port));
-                         }
-                         catch (ServerFailedToStart &exc)
-                         {
-                             config.displayError(exc);
-                             return;
-                         }
-                         config.hide();
+    try
+    {
+        serverApp = std::make_unique<ServerApplication>(std::make_unique<TCPMessageServer>(QHostAddress(ip), port));
+    }
+    catch (ServerFailedToStart &exc)
+    {
+        //  config.displayError(exc);
+        return EXIT_FAILURE;
+    }
 
-                         serverApp->add_chat(new ChatGui(serverApp.get(), GlobalChat::get_instance()));
+    serverApp->add_chat(new ChatGui(serverApp.get(), GlobalChat::get_instance()));
 
-                         serverApp->show();
-                     });
+    serverApp->show();
 
     return app.exec();
 }
