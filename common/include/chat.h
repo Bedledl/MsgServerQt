f915a414ed657fd13f26a6974b2ed1509a177d07
Bettina Riedl <bettina.riedl@zuhlke.com>
2024-01-31 09:03:02 +0100

    refactor Chat/ServerChat
        - get_key -> getKey to use CamelCase
        - make add Q_OBJECT macro
        - add ChatKey to Chat baseclass

--- common/include/chat.h
+++ common/include/chat.h
@@ -1,12 +1,16 @@
 #ifndef CHAT_H
 #define CHAT_H
 
+#include <QDate>
 #include <QObject>
 #include <QReadWriteLock>
 #include <functional>
 #include <set>
+#include <memory>
 
 #include "message.h"
+
+class Participant;
 
 typedef uint8_t ChatKey;
 
@@ -17,13 +21,25 @@ class Chat : public QObject
     std::set<Message> messages = {};
 
 public:
+    Chat(ChatKey key, QObject *parent = nullptr) : QObject(parent), key(key){};
     void foreach_do(std::function<void(const Message &)> func);
+    ChatKey getKey() const { return key; };
+    void addMessage(QString content, std::shared_ptr<Participant> sender, QDateTime timestamp)
+    {
+        sender;
+        timestamp;
+        // TODO
+        add_message(Message(content));
+    }
 
 public slots:
     void add_message(Message msg);
 
 signals:
     void messagesChanged();
+
+protected:
+    const ChatKey key;
 };
 
 #endif
