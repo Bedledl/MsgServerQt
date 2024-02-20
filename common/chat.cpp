332d4a10bf9a6e70e22c015b16e0777c816d9a15
Bettina Riedl <bettina.riedl@zuhlke.com>
2024-02-20 15:31:28 +0100

    refactor common/chat and add unit tests

--- common/chat.cpp
+++ common/chat.cpp
@@ -1,28 +1,101 @@
+#include <sstream>
+#include <stdexcept>
+
 #include "chat.h"
 
 #include "moc_chat.cpp"
 
 #include <algorithm>
 
-void Chat::add_message(Message msg)
+void Chat::addMessage(Message msg)
 {
+    mutex.lockForRead();
+    auto sender = msg.getSender();
+    if (!participants.contains(sender))
+    {
+        mutex.unlock();
+        throw SenderNotChatParticipant();
+    }
+
+    if (messages.contains(msg))
+    {
+        mutex.unlock();
+        return;
+    }
+
+    bool messageWasAppended = false;
+    if (messages.size() == 0 || msg > getLastMessage())
+    {
+        messageWasAppended = true;
+    }
+
+    mutex.unlock();
+
+    mutex.lockForWrite();
     messages.emplace(msg);
-    emit messagesChanged();
+    mutex.unlock();
+    if (messageWasAppended)
+    {
+        emit messageAppended();
+    }
+    else
+    {
+        emit messageOrderInvalidated();
+    }
+}
+void Chat::addMessage(QString content, std::shared_ptr<Participant> sender, QDateTime timestamp = QDateTime::currentDateTime())
+{
+    addMessage(Message(content, sender, timestamp));
+}
+
+void Chat::addParticipant(std::shared_ptr<Participant> participant)
+{
+    mutex.lockForWrite();
+    if (!participants.contains(participant))
+    {
+        participants.append(participant);
+    }
+    mutex.unlock();
+}
+
+void Chat::removeParticipant(std::shared_ptr<Participant> participant)
+{
+    mutex.lockForWrite();
+    participants.removeOne(participant);
+    mutex.unlock();
 }
 
 void Chat::foreach_do(std::function<void(const Message &)> func)
 {
+    mutex.lockForRead();
     std::for_each(messages.begin(), messages.end(), func);
+    mutex.unlock();
 }
 
-const Message *Chat::get_last_message() const
+const Message &Chat::getLastMessage() const
 {
+    mutex.lockForRead();
     qDebug() << "Chat::get_last_message()";
     if (messages.empty())
     {
-        qDebug() << "Chat::get_last_message() empty";
-        return nullptr;
+        mutex.unlock();
+        throw std::out_of_range("No messages in chat.");
     }
-    qDebug() << "Chat::get_last_message() not empty";
-    return &(*messages.begin());
+    mutex.unlock();
+    return getMessageAt(messages.size() - 1);
+}
+
+const Message &Chat::getMessageAt(size_t index) const
+{
+    mutex.lockForRead();
+    if (index < 0 || index >= messages.size())
+    {
+        mutex.unlock();
+        std::stringstream ss;
+        ss << "Message index " << index << " out of bounds.";
+        throw std::out_of_range(ss.str());
+    }
+    auto &r = *std::next(messages.begin(), index);
+    mutex.unlock();
+    return r;
 }
