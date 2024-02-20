112bcada5dddb491884339ed353d3eb3f8bfa616
Bettina Riedl <bettina.riedl@zuhlke.com>
2024-02-20 16:11:39 +0100

    fix remove more Widet Gui drafts

--- client/clientApp.cpp
+++
@@ -1,5 +0,0 @@
-#include "clientApp.h"
-#include "client.h"
-#include "moc_clientApp.cpp"
-
-ClientApp::ClientApp(std::unique_ptr<Client> client, QWidget *parent) : AppGui(parent), client(std::move(client)) {}
