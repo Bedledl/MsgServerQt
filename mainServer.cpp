170d09632f54ae6acbce126555672e618d8a302a
Bettina Riedl <bettina.riedl@zuhlke.com>
2024-02-21 08:16:29 +0100

    Cmd Line Options: make strings constexpr

--- mainServer.cpp
+++ mainServer.cpp
@@ -17,14 +17,12 @@ int main(int argc, char *argv[])
     QApplication::setApplicationName("LaunchServer");
     QApplication::setApplicationVersion("1.0");
 
-    QString configFileCmdLineOptName = "config-file";
-    QString pingingModeCmdLineOptName = "ping";
     QCommandLineParser parser;
-    configureParser(parser, configFileCmdLineOptName);
+    configureParser(parser);
     parser.process(app);
 
-    std::unique_ptr<ConnConfigurator> connConfigurator = createConnConfiguratorFromSettings(parser, configFileCmdLineOptName);
-    bool usePingCommunicator = parsePingConfig(parser, pingingModeCmdLineOptName);
+    std::unique_ptr<ConnConfigurator> connConfigurator = createConnConfiguratorFromSettings(parser);
+    bool usePingCommunicator = parsePingConfig(parser);
 
     // may block until conneciton configuration is retrieved
     auto [ip, port] = connConfigurator->retrieveConnectionConfiguration();
