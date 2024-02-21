991893a82bda7e3e2a5988cb83cc91f98839675f
Bettina Riedl <bettina.riedl@zuhlke.com>
2024-02-21 11:06:14 +0100

    add test infrastructure

---
+++ test/client/clientCommunicatorTest.cpp
@@ -0,0 +1,28 @@
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include <QString>
+
+#include <client.h>
+
+
+class MockClient : public Client
+{
+public:
+    MOCK_METHOD(void, addNewChat, (const ChatKey &key), ());
+    MOCK_METHOD(void, leaveChat, (const ChatKey &key), ());
+    MOCK_METHOD(void, addNewIncomingMessage, (const ChatKey &key, const QString &content, const ParticipantKey &participantKey, const QDateTime &timestamp), ());
+    MOCK_METHOD(void, assignParticipantName, (const ParticipantKey &key, const QString &name), ());
+    MOCK_METHOD(void, assignParticipantEntryDate, (const ParticipantKey &key, const QDateTime &entryDate), ());
+    MOCK_METHOD(QString, getNickname, (), ());
+    MOCK_METHOD(int, getPort, (), ());
+    MOCK_METHOD(QString, getIp, (), ());
+    MOCK_METHOD(int, selectedChat, (), ());
+    MOCK_METHOD(void, setSelectedChat, (int selectedChat), ());
+};
+
+class ClientCommunicatorTest : public ::testing::Test
+{
+protected:
+    MockClient client;
+};
