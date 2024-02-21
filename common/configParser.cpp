4795639af49307657aea704ec973ae3f798d5fc7
Bettina Riedl <bettina.riedl@zuhlke.com>
2024-02-21 08:16:29 +0100

    Cmd Line Options: make strings constexpr

--- common/configParser.cpp
+++ common/configParser.cpp
@@ -8,23 +8,23 @@
 #include <map>
 #include <variant>
 
-void configureParser(QCommandLineParser &parser, QString configFileCmdLineOptName)
+void configureParser(QCommandLineParser &parser)
 {
     parser.setApplicationDescription("Chat Program Command Line Parser");
     parser.addHelpOption();
     parser.addVersionOption();
 
-    QCommandLineOption configFileOption(QStringList() << configFileCmdLineOptName,
+    QCommandLineOption configFileOption(QStringList() << configFileCmdLineOptName.c_str(),
                                         QCoreApplication::translate("main", "Specfiy config file to configure connection with."),
                                         QCoreApplication::translate("main", "directory"));
-    QCommandLineOption pingOption(QStringList() << "ping", QCoreApplication::translate("main", "Enable pinging between server and client for debugging."));
+    QCommandLineOption pingOption(QStringList() << pingingModeCmdLineOptName.c_str(), QCoreApplication::translate("main", "Enable pinging between server and client for debugging."));
     parser.addOption(configFileOption);
     parser.addOption(pingOption);
 }
 
-std::unique_ptr<ConnConfigurator> createConnConfiguratorFromSettings(QCommandLineParser &parser, QString configFileCmdLineOptName)
+std::unique_ptr<ConnConfigurator> createConnConfiguratorFromSettings(QCommandLineParser &parser)
 {
-    QString configFilePath = parser.value(configFileCmdLineOptName);
+    QString configFilePath = parser.value(configFileCmdLineOptName.c_str());
     if (configFilePath.isEmpty())
     {
         return std::make_unique<ConnConfiguratorGraphic>();
@@ -33,7 +33,7 @@ std::unique_ptr<ConnConfigurator> createConnConfiguratorFromSettings(QCommandLin
     return std::make_unique<ConnFileConfigurator>(file);
 }
 
-bool parsePingConfig(QCommandLineParser &parser, QString pingingModeCmdLineOptName)
+bool parsePingConfig(QCommandLineParser &parser)
 {
-    return parser.isSet(pingingModeCmdLineOptName);
+    return parser.isSet(pingingModeCmdLineOptName.c_str());
 }
