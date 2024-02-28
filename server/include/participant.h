301bf0939eb63a55608a3f6eecc2d406f3e742bf
Bettina Riedl <bettina.riedl@zuhlke.com>
2024-02-28 11:54:36 +0100

    Participant: add and implement Chat management methods

--- server/include/participant.h
+++ server/include/participant.h
@@ -37,9 +37,41 @@ public:
         }
         /// @brief should be called from the Client thread when the clients sent a new message
         void newMessage(ChatKey chat_key, QString content) { qDebug("NotImplemented yet"); };
-        void getParticipantKeys(ChatKey chat_key) { qDebug("NotImplemented yet"); };
-        void leaveChat(ChatKey chat_key) { qDebug("NotImplemented yet"); }
+        void getParticipantKeys(ChatKey chatKey) { qDebug("NotImplemented yet"); };
+        void leaveChat(ChatKey chatKey)
+        {
+                auto chat = std::find_if(chats.begin(), chats.end(), [&chatKey](std::shared_ptr<ServerChat> &chat)
+                                         { return chat->getKey() == chatKey; });
+                if (chat != chats.end())
+                {
+                        (*chat)->removeParticipant(getKey());
+                        std::erase(chats, *chat);
+                }
+                else
+                {
+                        qDebug() << "ServerParticipant::LeaveChat: Chat not found";
+                        throw ChatNotFound();
+                }
+        }
+        void joinChat(std::shared_ptr<ServerChat> chat)
+        {
+                chat->addParticipant(std::make_shared<ServerParticipant>(*this));
+                chats.emplace_back(std::move(chat));
+        }
+        std::shared_ptr<ServerChat> getChat(ChatKey chatKey)
+        {
+                auto chat = std::find_if(chats.begin(), chats.end(), [&chatKey](std::shared_ptr<ServerChat> &chat)
+                                         { return chat->getKey() == chatKey; });
+                if (chat != chats.end())
+                {
+                        return *chat;
+                }
+                else
+                {
+                        qDebug() << "ServerParticipant::getChat: Chat not found";
+                        throw ChatNotFound();
+                }
+        }
 };
-
 
 #endif /* A48213B2_FC46_48DB_8994_0AC8E86EB7C5 */
