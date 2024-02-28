5ced4c7e3bc03baa9a563906678f3fa04011819c
Bettina Riedl <bettina.riedl@zuhlke.com>
2024-02-28 11:47:25 +0100

    Server: implement ServerIface

--- server/include/server.h
+++ server/include/server.h
@@ -1,11 +1,12 @@
 #ifndef SERVER_H
 #define SERVER_H
 #include "chat.h"
-#include "clientThreadWorker.h"
+#include "participant.h"
 #include "serverIface.h"
 #include <exception>
 #include <memory>
 
+class Worker;
 // https://doc.qt.io/qt-6/qtnetwork-threadedfortuneserver-example.html
 
 class ServerFailedToStart : public std::exception
@@ -23,17 +24,24 @@ public:
     {
         globalChat = std::make_shared<ServerChat>();
     }
-    virtual ~Server(){
-
-    };
-    std::shared_ptr<ServerChat> getGlobalChat()
+    std::shared_ptr<ServerChat> getGlobalChat() const
     {
         return globalChat;
     }
+    void removeParticipantFromChat(ChatKey key, ParticipantKey participantKey) const override;
+    void addParticipantToChat(std::shared_ptr<ServerChat> chat, ParticipantKey participantKey) const override;
+    std::string getParticipantName(ParticipantKey key) const override;
+    void setParticipantName(ParticipantKey key, std::string name) const override;
+    QDateTime getParticipantEntryDate(ParticipantKey key) const override;
+    void setParticipantEntryDate(ParticipantKey key, QDateTime entryDate) const override;
+    void addNewIncomingMessage(ChatKey key, std::string content, ParticipantKey participantKey, QDateTime timestamp) const override;
+    void registerParticipant(std::shared_ptr<ServerParticipant> participant);
+    void unregisterParticipant(ParticipantKey key);
 
 protected:
     void create_new_client_thread(Worker *worker);
     std::shared_ptr<ServerChat> globalChat;
+    std::map<ParticipantKey, std::shared_ptr<ServerParticipant>> participants;
 };
 
 #endif
--- server/server.cpp
+++ server/server.cpp
@@ -1,5 +1,9 @@
-#include "server.h"
+#include <algorithm>
+#include <map>
+#include <vector>
+
 #include "clientThreadWorker.h"
+#include "server.h"
 #include <QThread>
 
 void Server::create_new_client_thread(Worker *worker)
@@ -12,4 +16,108 @@ void Server::create_new_client_thread(Worker *worker)
     thread->connect(worker, &Worker::finished, thread, &QThread::quit);
 
     thread->start();
+}
+
+void Server::registerParticipant(std::shared_ptr<ServerParticipant> participant)
+{
+    if (participants.contains(participant->getKey()))
+    {
+        throw ParticipantAlreadyExists();
+    }
+    else
+    {
+        participants.emplace(participant->getKey(), std::move(participant));
+    }
+}
+void Server::unregisterParticipant(ParticipantKey key)
+{
+    auto countRemoved = participants.erase(key);
+    if (countRemoved == 0)
+    {
+        throw ParticipantNotFound();
+    }
+}
+void Server::removeParticipantFromChat(ChatKey key, ParticipantKey participantKey) const
+{
+    auto participant = participants.find(participantKey);
+    if (participant != participants.end())
+    {
+        (*participant).second->leaveChat(key);
+    }
+    else
+    {
+        throw ParticipantNotFound();
+    }
+}
+void Server::addParticipantToChat(std::shared_ptr<ServerChat> chat, ParticipantKey participantKey) const
+{
+    auto participant = participants.find(participantKey);
+    if (participant != participants.end())
+    {
+        (*participant).second->joinChat(std::move(chat));
+    }
+    else
+    {
+        throw ParticipantNotFound();
+    }
+}
+std::string Server::getParticipantName(ParticipantKey key) const
+{
+    auto participant = participants.find(key);
+    if (participant != participants.end())
+    {
+        return (*participant).second->getNickname().toStdString();
+    }
+    else
+    {
+        throw ParticipantNotFound();
+    }
+}
+void Server::setParticipantName(ParticipantKey key, std::string name) const
+{
+    auto participant = participants.find(key);
+    if (participant != participants.end())
+    {
+        (*participant).second->setNickname(QString::fromStdString(name));
+    }
+    else
+    {
+        throw ParticipantNotFound();
+    }
+}
+QDateTime Server::getParticipantEntryDate(ParticipantKey key) const
+{
+    auto participant = participants.find(key);
+    if (participant != participants.end())
+    {
+        return (*participant).second->getEntryDate();
+    }
+    else
+    {
+        throw ParticipantNotFound();
+    }
+}
+void Server::setParticipantEntryDate(ParticipantKey key, QDateTime entryDate) const
+{
+    auto participant = participants.find(key);
+    if (participant != participants.end())
+    {
+        (*participant).second->setEntryDate(entryDate);
+    }
+    else
+    {
+        throw ParticipantNotFound();
+    }
+}
+void Server::addNewIncomingMessage(ChatKey key, std::string content, ParticipantKey participantKey, QDateTime timestamp) const
+{
+    auto participant = participants.find(participantKey);
+    if (participant != participants.end())
+    {
+        (*participant).second->newMessage(key, QString::fromStdString(content));
+    }
+    else
+    {
+        throw ParticipantNotFound();
+    }
 }
