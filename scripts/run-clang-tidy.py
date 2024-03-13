36dda80a95bc62275f74335189f56665798ad44b
Bettina Riedl <bettina.riedl@zuhlke.com>
2024-03-13 11:39:04 +0100

    Add clang tidy config file and script

---
+++ scripts/run-clang-tidy.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+
+# The CI pipeline does currently not use this scripts, but a public github action for clang tidy. This script is kept for local development.
+
+# This script analyzes the code with clang-tidy according to the rules in the .clang-tidy file. An optional parameter can be passed to try to fix errors in place.
+# Usage: ./scripts/run-clang-tidy.py [fix]
+
+from os import path, cpu_count
+from glob import glob
+from subprocess import run, PIPE
+from distutils.spawn import find_executable
+from concurrent.futures import ThreadPoolExecutor
+
+# Check if clang-tidy is installed
+if not find_executable("clang-tidy"):
+    print("Error: clang-tidy is not installed.")
+    exit(1)
+
+# Check if .clang-tidy file exists
+if not path.isfile(".clang-tidy"):
+    print("Error: .clang-tidy file not found.")
+    exit(1)
+
+# Check if an optional parameter is passed to try to fix erros in place
+# if [ "$1" == "fix" ]; then
+#   echo "Try to fix errors in place"
+#   RUN_MODE="-i"
+# else
+#   RUN_MODE="--dry-run"
+# fi
+
+# First, get all of the source files and headers to be analyzed.
+files = glob('**/*.cpp', recursive=True)
+files += glob('**/*.h', recursive=True)
+files += glob('**/*.hpp', recursive=True)
+
+files = list(filter(lambda file: not file.startswith("build/") and not file.startswith("CMake"), files))
+
+print(files)
+
+# Define output files
+TMP_FILES_LIST_FILE=".tmp-clang-tidy-files-list"
+TMP_REPORT_FILE=".tmp-clang-tidy-report"
+
+# Ensure that output files are empty, so without previous log data
+# echo -n "" >${TMP_REPORT_FILE}
+# echo -n "" >${TMP_FILES_LIST_FILE}
+
+def run_clang_tidy(file):
+  print("Starting clang-tidy for file: " + file)
+  clang_tidy_result = run(['clang-tidy', '-p', 'build', '-extra-arg=-std=c++17', '--config-file=.clang-tidy', file], stdout=PIPE)
+  # TODO store result in file
+  print("Finished clang-tidy for file: " + file)
+  print(clang_tidy_result.stdout.decode('utf-8'))
+
+
+with ThreadPoolExecutor(max_workers=cpu_count()) as executor:
+    for file in files:
+       future = executor.submit(run_clang_tidy, file)
+
+
+# if [ ! -s "$TMP_REPORT_FILE" ]; then
+#   # report file is empty
+#   rm $TMP_REPORT_FILE
+#   exit 0
+# fi
+
+# echo "clang-tidy check failed, see report in $TMP_REPORT_FILE. The list of files that were analyzed is in $TMP_FILES_LIST_FILE"
+# exit 1
