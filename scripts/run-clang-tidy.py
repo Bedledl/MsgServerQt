f40e84aead801eeb2c23ee343ae100d666aac6c9
Bettina Riedl <bettina.riedl@zuhlke.com>
2024-03-20 13:52:21 +0100

    CI: add build-folder parameter to clang tidy script

--- scripts/run-clang-tidy.py
+++ scripts/run-clang-tidy.py
@@ -8,6 +8,7 @@
 from os import path, cpu_count
 from glob import glob
 from subprocess import run, PIPE
+from sys import argv
 from distutils.spawn import find_executable
 from concurrent.futures import ThreadPoolExecutor
 
@@ -22,12 +23,18 @@ if not path.isfile(".clang-tidy"):
     exit(1)
 
 # Check if an optional parameter is passed to try to fix erros in place
-# if [ "$1" == "fix" ]; then
-#   echo "Try to fix errors in place"
-#   RUN_MODE="-i"
-# else
-#   RUN_MODE="--dry-run"
-# fi
+fix_inplace = ""
+build_folder = "build"
+for arg in argv[1:]:
+    if arg == "--fix":
+        fix_inplace = "-fix-errors"
+        continue
+    elif arg.startswith("--build="):
+        build_folder = arg.split("=")[1]
+        continue
+    else:
+        print("Error: Invalid argument. Please pass '--fix' to try to fix errors in place or '--build=<build_folder>' to specify the build folder.")
+        exit(1)
 
 # First, get all of the source files and headers to be analyzed.
 files = glob('**/*.cpp', recursive=True)
@@ -48,7 +55,7 @@ TMP_REPORT_FILE=".tmp-clang-tidy-report"
 
 def run_clang_tidy(file):
   print("Starting clang-tidy for file: " + file)
-  clang_tidy_result = run(['clang-tidy', '-p', 'build', '-extra-arg=-std=c++17', '--config-file=.clang-tidy', file], stdout=PIPE)
+  clang_tidy_result = run(['clang-tidy', '-p', build_folder, fix_inplace, '-extra-arg=-std=c++17', '--config-file=.clang-tidy', file], stdout=PIPE)
   # TODO store result in file
   print("Finished clang-tidy for file: " + file)
   print(clang_tidy_result.stdout.decode('utf-8'))
